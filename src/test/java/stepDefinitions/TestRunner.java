5da494e510f74d11a2394cc53afd32b6c7692157
RaghavendraKumarVasa <vasaraghavendra@gmail.com>
2022-04-25 19:54:36 +0530

    removed the encoding and compiler version of maven, also changed the reports location

--- src/test/java/stepDefinitions/TestRunner.java
+++ src/test/java/stepDefinitions/TestRunner.java
@@ -4,14 +4,15 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features="src/test/resources/features", glue= {"stepDefinitions"},
+        features = "src/test/resources/features", glue = {"stepDefinitions"},
         //tags="@test",
         //dryRun = false,
-        monochrome=true,
-        plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "pretty","html:reports/CucumberReports/CucumberHtmlReport.html",
-                "json:reports/CucumberReports/CucumberJsonReport.json",
-                "junit:reports/CucumberReports/CucumberJunitReport.xml"}
+        monochrome = true,
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "pretty", "html: CucumberHtmlReport.html"
+//                ,"json:reports/CucumberReports/CucumberJsonReport.json",
+//                "junit:reports/CucumberReports/CucumberJunitReport.xml"
+        }
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
